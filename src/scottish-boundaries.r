39b474ef73eff7b0899841891dcb7dc6bbc6885f
Phil Mike Jones <philmikejones@gmail.com>
2024-09-15 11:12:46 +0100

    update scottish-boundaries.r code

--- src/scottish-boundaries.r
+++ src/scottish-boundaries.r
@@ -1,29 +1,45 @@
+library("assertthat")
 library("dplyr")
 library("sf")
 library("ggplot2")
 
 ca = sf::read_sf("data/scotland_ca_2019.shp")
-oa = sf::read_sf("data/scotland_oa_2022.shp")
+ca = rmapshaper::ms_simplify(ca, keep = 0.05)
+
+oa = 
+    sf::read_sf("data/scotland_oa_2022.shp") |>
+    select(geoid, council) |>
+    st_drop_geometry()
 
 ethnic = 
     readr::read_csv("data/UV201 - Ethnic group.csv", skip = 4, na = "-", show_col_types = FALSE) |>
     rename(
+        oa_code = code,
         total = `All People`,
         irish = `White: White Irish`
     ) |>
-    select(code, irish, total) |>
-    mutate(irish = if_else(is.na(irish), 0, irish)) |>
-    mutate(p_irish = irish / total)
+    select(oa_code, irish, total) |>
+    mutate(irish = if_else(is.na(irish), 0, irish))
 
-oa = left_join(oa, ethnic, by = c("geoid" = "code"))
+ethnic =
+    left_join(oa, ethnic, by = c("geoid" = "oa_code")) |>
+    group_by(council) |>
+    summarise(irish = sum(irish), total = sum(total)) |>
+    ungroup()
 
-print(oa)
-saveRDS(oa, file = "oa.rds", compress = FALSE)
+assertthat::assert_that(assertthat::are_equal(
+    nrow(anti_join(ca, ethnic, by = c("geoid" = "council"))),
+    0L
+))
+
+assert_that(are_equal(56766L, sum(ethnic$irish)))  # manual count from spreadsheet
+
+ca = left_join(ca, ethnic, by = c("geoid" = "council"))
+
+ca =
+    ca |>
+    mutate(`% White Irish` = (irish / total) * 100)
 
 map = ggplot(data = ca) +
-    geom_sf()
-ggsave("figures/scot-ca.pdf", map, width = 210, height = 297, units = "mm", dpi = 300)
-
-# map = ggplot(data = oa) +
-#     geom_sf(aes(fill = p_irish))
-# ggsave("figures/scot-oa-irish.pdf", map, width = 210, height = 297, units = "mm", dpi = 300)
+    geom_sf(aes(fill = `% White Irish`))
+ggsave("figures/scot-ca-irish.pdf", map, width = 210, height = 297, units = "mm", dpi = 300)
